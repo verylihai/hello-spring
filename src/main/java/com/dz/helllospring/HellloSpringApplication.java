d24b8732b34b9724c6d172c94132873e7cdd416e
verylihai <510216968@qq.com>
2023-05-10 22:21:47 +0800

    init repo

---
+++ src/main/java/com/dz/helllospring/HellloSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dz.helllospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellloSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellloSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dz/helllospring/HellloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dz.helllospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellloSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
